9fc14d5eb9ad43c592aa8ad68245d4e62b28c814
Anisa <anisahamohamed@gmail.com>
2020-03-10 11:59:38 -0400

    personalize blog

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -2,7 +2,7 @@
   <transition appear name="v--mask">
     <footer v-once class="blog__footer">
       <div class="signature cosmicjs">
-        Proudly powered by <a href="https://cosmicjs.com" target="_blank">Cosmic JS</a>
+        By <a href="https://github.com/AnisaHMohamed" target="_blank">Anisa Mohamed</a>
       </div>
     </footer>
   </transition>
